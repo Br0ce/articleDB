eb6c2f04b605ca6bb84280156ae18a53b0a576f3
nbeck <niklas.beck@iais.fraunhofer.de>
2023-11-01 18:45:48 +0100

    Do not use noop client (will be replaced)

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/Br0ce/articleDB/pkg/adder"
 	"github.com/Br0ce/articleDB/pkg/db/inmem"
-	"github.com/Br0ce/articleDB/pkg/extract/noop"
 )
 
 type Api struct {
@@ -16,11 +15,10 @@ type Api struct {
 
 func New(log *slog.Logger) (*Api, error) {
 	db := inmem.NewArticle()
-	noop := noop.Client{}
 
 	_, err := adder.New(
-		adder.WithSummarizer(noop),
-		adder.WithNamedEntityRecognizer(noop),
+		// adder.WithSummarizer(noop),
+		// adder.WithNamedEntityRecognizer(noop),
 		adder.WithDB(db),
 		adder.WithLogger(log.With("name", "api")),
 	)
